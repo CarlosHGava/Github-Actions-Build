08367867b2f4782fd616a199a9f8fcf49e5c1a8b
CarlosHGava <c.gava@grouplinkone.com>
2025-08-12 09:17:58 -0300

    Initial Commit

---
+++ Github Actions Build/Github_Actions_BuildApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Actions_BuildApp.swift
+//  Github Actions Build
+//
+//  Created by Carlos Gava on 12/08/25.
+//
+
+import SwiftUI
+
+@main
+struct Github_Actions_BuildApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Github Actions BuildTests/Github_Actions_BuildTests.swift
@@ -0,0 +1,17 @@
+//
+//  Github_Actions_BuildTests.swift
+//  Github Actions BuildTests
+//
+//  Created by Carlos Gava on 12/08/25.
+//
+
+import Testing
+@testable import Github_Actions_Build
+
+struct Github_Actions_BuildTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
